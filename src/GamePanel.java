576e0fa6a735b9383addceae1eb104e8fbf5c5fd
kanvar22 <league@WorthyMini-5.attlocal.net>
2017-02-16 17:01:58 -0800

    I worked on Pt.2-pt.5 and on Pt. 5 I am working on step 6. Next class I am going to work on as many Pt.'s as I can.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,17 +1,44 @@
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener{
-Timer timer; 
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
+Timer timer;
+
 	public GamePanel(){
-	
+	timer = new Timer(1000/60, this);
 }
+public void startGame(){
+	timer.start();
+}
+public void paintComponent(Graphics g){
+	g.fillRect(10, 10, 100, 100);
+
+}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		repaint();
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("keyTyped");
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("keyPressed");
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("keyReleased");
 	}
 }
--- src/Pacman.java
+++ src/Pacman.java
@@ -4,18 +4,24 @@ public class Pacman {
 JFrame frame;
 final int width = 961;
 final int height = 488;
+GamePanel game;
 static Pacman pacman;
 	
-	Pacman(){
+	public Pacman(){
 		frame = new JFrame();
+		game = new GamePanel();
 		setup();
-		pacman = new Pacman();
 }
-	public static void main(String[] args) {
-		
+	public static void main(String [] args) {
+		pacman = new Pacman();
 	}
 	
 	public void setup (){
+		frame.add(game);
+		game.startGame();
+		frame.addKeyListener(game);
+		frame.setSize(width, height);
+		frame.setVisible(true);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 	}
 }
