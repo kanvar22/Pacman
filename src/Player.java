babffebed9875be8d50be9bd343063f56a8ba8bf
kanvar22 <league@WorthyMini-5.attlocal.net>
2017-04-20 17:00:21 -0700

    next class i will fix the bug on my color detection

--- src/Player.java
+++ src/Player.java
@@ -6,8 +6,8 @@ public static boolean up = false;
  public static boolean down = false;
  public static boolean right = false;
  public static boolean left = false;
- final int widthP = 10;
- final int heightP = 10;
+ final int widthP = 8;
+ final int heightP = 8;
 boolean inBoundsFL = true;
 boolean inBoundsFR = true;
 boolean inBoundsBL = true;
@@ -26,17 +26,56 @@ boolean inBoundsBR = true;
 		System.out.println(x + " " + y);
 	System.out.println(	GamePanel.MazeImg.getHeight());
 
-	//for example, if pacman is heading right decrease x by 1.
+	//Color Detection
 		int a = GamePanel.MazeImg.getRGB(x, y);
-		int b = GamePanel.MazeImg.getRGB(x + 10, y);
-		int c = GamePanel.MazeImg.getRGB(x, y + 10);
-		int d = GamePanel.MazeImg.getRGB(x + 10, y + 10);
+		int b = GamePanel.MazeImg.getRGB(x + widthP, y);
+		int c = GamePanel.MazeImg.getRGB(x, y + heightP);
+		int d = GamePanel.MazeImg.getRGB(x + widthP, y + heightP);
 		System.out.println(GamePanel.MazeImg.getRGB(x, y));
 		
-		if (a == -16777216) {
-			inBoundsFL = true;
+		if (a != -16777216 && b != -16777216) {
+			inBoundsFL = false;
+			inBoundsFR = false;
 		}
 		
+		if (b != -16777216 && d != -16777216) {
+			inBoundsFR = false;
+			inBoundsBR = false;
+		}
+		
+		if (d != -16777216 && c != -16777216) {
+			inBoundsBR = false;
+			inBoundsBL = false;
+		}
+		
+		if (c != -16777216 && a != -16777216) {
+			inBoundsBL = false;
+			inBoundsFL = false;
+		}
+		
+		if (inBoundsFL == false && inBoundsFR ==  false) {
+			up = false;
+			inBoundsFL = true;
+			inBoundsFR = true;
+		}
+		
+		if (inBoundsFR == false && inBoundsBR == false) {
+			right = false;
+			inBoundsFR = true;
+			inBoundsBR = true;
+		}
+
+		if (inBoundsBR == false && inBoundsBL == false) {
+			down = false;
+			inBoundsBL = true;
+			inBoundsBR = true;
+		}
+
+		if (inBoundsBL == false && inBoundsFL == false) {
+			left = false;
+			inBoundsBL = true;
+			inBoundsFL = true;
+		}
 		if (up == true) {
 			y = y - 1;
 		}
@@ -52,17 +91,18 @@ boolean inBoundsBR = true;
 			
 			x = x + 1;
 		}
-	if (y >= GamePanel.MazeImg.getHeight() - heightP-1) {
-		y = 1;
-	}
-	if (y <= 0) {
-		y = GamePanel.MazeImg.getHeight() - heightP-1;
-	}
-	if (x >= GamePanel.MazeImg.getWidth() - widthP - 1) {
-		x = 1;
-	}
-	if (x <= 0) {
-		x = GamePanel.MazeImg.getWidth() - widthP - 1;
-	}
+	//Movement of Pacman
+		if (y >= GamePanel.MazeImg.getHeight() - heightP-1) {
+			y = 1;
+		}
+		if (y <= 0) {
+			y = GamePanel.MazeImg.getHeight() - heightP-1;
+		}
+		if (x >= GamePanel.MazeImg.getWidth() - widthP - 1) {
+			x = 1;
+		}
+		if (x <= 0) {
+			x = GamePanel.MazeImg.getWidth() - widthP - 1;
+		}
 	}
 }
