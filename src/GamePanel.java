57f5abc98bbda64b57f4343530664c11e97d8470
NolanPlatt <league@WorthyMini-5.attlocal.net>
2017-04-27 17:04:22 -0700

    Next class I will work on making my enemy

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -17,6 +17,7 @@ public class GamePanel extends JPanel {
 	public static BufferedImage pacmanImg;
 	public static BufferedImage MazeImg;
 	public static BufferedImage WallImg;
+	public static BufferedImage CherryImg;
 	GameObject object;
 
 	public GamePanel(){
@@ -25,6 +26,8 @@ public class GamePanel extends JPanel {
 			pacmanImg = ImageIO.read(this.getClass().getResourceAsStream("Pacman.png"));
 			MazeImg = ImageIO.read(this.getClass().getResourceAsStream("Maze.png"));
 			WallImg = ImageIO.read(this.getClass().getResourceAsStream("Wall for guy.png"));
+			CherryImg = ImageIO.read(this.getClass().getResourceAsStream("Cherry.png"));
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,9 +38,9 @@ public class GamePanel extends JPanel {
 	public void paintComponent(Graphics g){
 		g.setColor(Color.black);
 		g.drawImage(GamePanel.MazeImg, 0, 0, 961, 488, null);
-		g.drawImage(GamePanel.WallImg, 10, 510, 10, 10, null);
-		g.drawImage(GhostImg, 100, 80, 20, 20, null);
-		g.drawImage(pacmanImg, 10, 10, 2, 2, null);
+		//g.drawImage(GamePanel.WallImg, 10, 510, 10, 10, null);
+		//g.drawImage(GhostImg, 100, 80, 20, 20, null);
+		//g.drawImage(pacmanImg, 10, 10, 2, 2, null);
 		object.draw(g);
 	}
 
--- src/Player.java
+++ src/Player.java
@@ -8,10 +8,17 @@ public static boolean up = false;
  public static boolean left = false;
  final int widthP = 8;
  final int heightP = 8;
+//Regular boolean
 boolean inBoundsFL = true;
 boolean inBoundsFR = true;
 boolean inBoundsBL = true;
 boolean inBoundsBR = true;
+//Side Boolean
+boolean inBoundsSML = true;
+boolean inBoundsSMR = true;
+boolean inBoundsBM = true;
+boolean inBoundsFM = true;
+
  
 	public Player(){
 		x = 21;
@@ -31,6 +38,10 @@ boolean inBoundsBR = true;
 		int b = GamePanel.MazeImg.getRGB(x + widthP, y);
 		int c = GamePanel.MazeImg.getRGB(x, y + heightP);
 		int d = GamePanel.MazeImg.getRGB(x + widthP, y + heightP);
+		int e = GamePanel.MazeImg.getRGB(x + 4, y);
+		int f = GamePanel.MazeImg.getRGB(x , y + 4);
+		int g = GamePanel.MazeImg.getRGB(x + 8, y + 4);
+		int h = GamePanel.MazeImg.getRGB(x + 4, y + 8);
 		System.out.println(GamePanel.MazeImg.getRGB(x, y));
 		
 		if (a != -16777216 && b != -16777216) {
@@ -53,29 +64,60 @@ boolean inBoundsBR = true;
 			inBoundsFL = false;
 		}
 		
+		if (e != -16777216) {
+			inBoundsFM = false;
+		}
+		
+		if (f != -16777216) {
+			inBoundsSML = false;
+		}
+		
+		if (g != -16777216) {
+			inBoundsSMR = false;
+		}
+		
+		if (h != -16777216) {
+			inBoundsBM = false;
+		}
+		
 		if (inBoundsFL == false && inBoundsFR ==  false) {
 			up = false;
-			inBoundsFL = true;
-			inBoundsFR = true;
+		
 		}
 		
 		if (inBoundsFR == false && inBoundsBR == false) {
 			right = false;
-			inBoundsFR = true;
-			inBoundsBR = true;
+		
 		}
 
 		if (inBoundsBR == false && inBoundsBL == false) {
 			down = false;
-			inBoundsBL = true;
-			inBoundsBR = true;
+	
 		}
 
 		if (inBoundsBL == false && inBoundsFL == false) {
 			left = false;
-			inBoundsBL = true;
-			inBoundsFL = true;
+		
 		}
+		
+		if (inBoundsFM == false) {
+			up = false;
+		}
+		
+		if (inBoundsBL == false) {
+			down = false;
+		}
+		
+		if (inBoundsSML == false) {
+			left = false;
+		}
+		
+		if (inBoundsSMR == false) {
+			right = false;
+		}
+		
+		
+		
 		if (up == true) {
 			y = y - 1;
 		}
@@ -91,6 +133,15 @@ boolean inBoundsBR = true;
 			
 			x = x + 1;
 		}
+		inBoundsFL = true;
+		inBoundsFR = true;
+		inBoundsBL = true;
+		inBoundsBR = true;
+		inBoundsFM = true;
+		inBoundsSMR = true;
+		inBoundsSML = true;
+		inBoundsBM = true;
+		
 	//Movement of Pacman
 		if (y >= GamePanel.MazeImg.getHeight() - heightP-1) {
 			y = 1;
